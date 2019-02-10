4b11f123c3e0aa20df2211416cd1de0861838037
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-10 15:44:13 +0900

    add comments :edit & :update #25

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'topics#index'
   resources :topics do
-    resources :comments, :only => [:create, :destory]
+    resources :comments, :only => [:edit, :create, :update, :destory]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
