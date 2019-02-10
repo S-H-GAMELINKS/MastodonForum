6e0fea1fb346f4441de21f73b85bf6b2c913c0af
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-10 13:25:18 +0900

    Migrate DB #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_041709) do
+ActiveRecord::Schema.define(version: 2019_02_10_042314) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
