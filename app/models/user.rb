6e36e81952ff4fe784b5f6f98396de6e1e26f7e4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-10 20:12:02 +0900

    get username #42

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,14 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    if user != nil then
+      user.update!(name: auth.extra.raw_info.display_name)
+    end
       
     unless user
       user = User.create(
+        name:     auth.extra.raw_info.display_name,
         uid:      auth.uid,
         provider: auth.provider,
         email:    User.dummy_email(auth),
