ee4360518be878867549bd8aa184bfca42b5330f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-10 10:36:34 +0900

    using comments route #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do
+    resources :comments, :only => [:create, :destory]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
