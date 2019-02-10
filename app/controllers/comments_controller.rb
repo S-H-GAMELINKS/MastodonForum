8aea42ea48e60694bcdf65c5db59199fc2aa5161
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-10 10:41:32 +0900

    add comments_controller.rb #4

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+    before_action :set_post
+
+    def create
+        @topic.comments.create! comments_params
+        redirect_to @topic
+    end
+
+    def destroy
+        @topic.comments.destroy params[:id]
+        redirect_to @topic
+    end
+
+     private
+        def set_post
+            @topic = Topic.find(params[:post_id])
+        end
+
+         def comments_params
+            params.required(:comment).permit(:content)
+        end
+end
