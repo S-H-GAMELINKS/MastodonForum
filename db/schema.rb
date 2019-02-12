d3bac287950c3dcc00b9db62b682db94a6ee3fc0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-12 18:21:55 +0900

    Migrate DB #41

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_10_110915) do
+ActiveRecord::Schema.define(version: 2019_02_12_092056) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_02_10_110915) do
     t.string "uid"
     t.string "provider"
     t.string "name"
+    t.string "icon"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
